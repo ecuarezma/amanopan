d2a1073cab398a39a9988feeb336dbe6cd405dce
Edgar Cuarezma <ecuarezma@gmail.com>
2022-08-12 09:49:37 -0500

    removed header calling for now hiring

--- src/Content/index.js
+++ src/Content/index.js
@@ -7,14 +7,6 @@ const Content = () => {
     {/* <ul>
       <li><a href="https://cashdrop.biz/amanopan">PREORDER ONLINE</a></li>
     </ul> */}
-    <div>
-    <ul>
-      <li><div >
-      <h1>NOW HIRING!</h1>
-      <h4><a href="https://form.jotform.com/212738155151149">apply now!</a></h4>
-    </div></li>
-    </ul>
-    </div>
     
     <div className={classes.Wrapper}>
       <div className={classes.Video}>

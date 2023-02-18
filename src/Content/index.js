2518fe0bf71490cd6ea4b9a54a608f6773ed3974
Edgar Cuarezma <ecuarezma@gmail.com>
2023-02-18 12:51:50 -0600

    Fix video playback issues

--- src/Content/index.js
+++ src/Content/index.js
@@ -1,24 +1,19 @@
 import React from "react";
 import classes from "./Content.module.scss";
-import ReactPlayer from "react-player";
+import Vimeo from "@u-wave/react-vimeo";
 
 const Content = () => {
-  return <div className={classes.Content}>
-    {/* <ul>
+  return (
+    <div className={classes.Content}>
+      {/* <ul>
       <li><a href="https://cashdrop.biz/amanopan">PREORDER ONLINE</a></li>
     </ul> */}
-    
-    <div className={classes.Wrapper}>
+
       <div className={classes.Video}>
-      <ReactPlayer
-        width="100%"
-        url="https://vimeo.com/555433247"
-      />
+        <Vimeo video="https://vimeo.com/555433247" autoplay />
       </div>
     </div>
-    
-    
-    </div>;
+  );
 };
 
 export default Content;
